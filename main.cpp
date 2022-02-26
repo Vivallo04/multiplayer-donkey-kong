46a781b8949869ba775b4a7d7276261c4151f290
Vivallo04 <andresvivallo4@gmail.com>
2022-02-26 11:27:39 -0600

    Added base window

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <SFML/Window.hpp>
 
 int main()
 {
     std::cout << "Hello, World!" << std::endl;
+
+    sf::Window window(sf::VideoMode(1280, 720), "Multiplayer Donkey Kong");
+    while (window.isOpen())
+    {
+        sf::Event event{};
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+    }
     return 0;
 }
